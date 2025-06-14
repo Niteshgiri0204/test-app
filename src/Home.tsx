61355ba12f298fe9ba87eb93ddc22fab02c96867
nitesh02 <nitugiri02@gmail.com>
2025-06-14 12:15:57 +0530

    Initial React + Vite App

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>Welcome to My React App</h1>
+      <p>This is a single page hosted on GitHub Pages!</p>
+    </div>
+  );
+};
+
+export default Home;
